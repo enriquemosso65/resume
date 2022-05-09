8fc41cff43af38a8ef124668f2547a96acb34c5b
enriquemosso65 <enriquemosso65@aragon.unam.mx>
2022-05-09 18:44:24 -0500

    publicación portafolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const Locoscroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    smartphone: {
+          smooth: true
+      }
+    //reloadOnContextChange: true
+  });
